d98234c87942cce4ae0f458b699a3aed047d278d
Beth <bethgranados@gmail.com>
2021-09-25 09:45:17 -0500

    Moved entire block into its own class and got them into tabs

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ package main
 import (
 	"os"
 
-	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 )
@@ -102,17 +101,129 @@ type Damage_blocker interface {
 }
 
 type damage_block struct {
+	Damage1Group          *widgets.QGroupBox
+	Damage1ComboBoxTF     *widgets.QComboBox
+	Damage1LabelDamage    *widgets.QLabel
+	Damage1LineEditDamage *widgets.QLineEdit
+	Damage1Plus1          *widgets.QLabel
+	Damage1ComboBoxType   *widgets.QComboBox
+	Damage1LabelPlus2     *widgets.QLabel
+	Damage1LineEditBonus  *widgets.QLineEdit
+
+	Damage1LabelType    *widgets.QLabel
+	Damage1LineEditType *widgets.QLineEdit
+	Damage1LabelCrit    *widgets.QLabel
+	Damage1LineEditCrit *widgets.QLineEdit
+	damage1Layout       *widgets.QGridLayout
 }
 
 func create_damage_block() *damage_block {
-	v := damage_block{}
+	v := damage_block{widgets.NewQGroupBox2("", nil), widgets.NewQComboBox(nil), widgets.NewQLabel2("Damage:", nil, 0), widgets.NewQLineEdit(nil), widgets.NewQLabel2("+", nil, 0), widgets.NewQComboBox(nil), widgets.NewQLabel2("+", nil, 0), widgets.NewQLineEdit(nil), widgets.NewQLabel2("Type:", nil, 0), widgets.NewQLineEdit(nil), widgets.NewQLabel2("Crit:", nil, 0), widgets.NewQLineEdit(nil), widgets.NewQGridLayout2()}
 	return &v
 }
 
-//
+func (x damage_block) setup() {
+	x.Damage1ComboBoxTF.AddItems([]string{"True", "False"})
+	x.Damage1ComboBoxType.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA", "SPELL", "-"})
+	x.Damage1LineEditDamage.SetPlaceholderText("1d6")
+	x.Damage1LineEditBonus.SetPlaceholderText("0")
+	x.Damage1LineEditType.SetPlaceholderText("Slashing")
+	x.Damage1LineEditCrit.SetPlaceholderText("1d6")
+
+	x.damage1Layout.AddWidget2(x.Damage1ComboBoxTF, 0, 0, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LabelDamage, 0, 1, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LineEditDamage, 0, 2, 0)
+	x.damage1Layout.AddWidget2(x.Damage1Plus1, 0, 3, 0)
+	x.damage1Layout.AddWidget2(x.Damage1ComboBoxTF, 0, 4, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LabelPlus2, 0, 5, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LineEditBonus, 0, 6, 0)
+
+	x.damage1Layout.AddWidget2(x.Damage1LabelType, 1, 0, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LineEditType, 1, 1, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LabelCrit, 1, 2, 0)
+	x.damage1Layout.AddWidget2(x.Damage1LineEditCrit, 1, 3, 0)
+
+	x.Damage1Group.SetLayout(x.damage1Layout)
+}
+
+// Saving Throw Box
+
+type saving_throw interface {
+	setup()
+}
+
+type saving_throw_block struct {
+	SavingThrowGroup            *widgets.QGroupBox
+	SavingThrowComboBoxTF       *widgets.QComboBox
+	SavingThrowLabelSavingThrow *widgets.QLabel
+	SavingThrowComboBoxStat     *widgets.QComboBox
+	SavingThrowLabelVsDC        *widgets.QLabel
+	SavingThrowComboBoxType     *widgets.QComboBox
+
+	SavingThrowLabelEffect *widgets.QLabel
+	SavingThrowLineEdit    *widgets.QLineEdit
+	SavingThrowLayout      *widgets.QGridLayout
+}
+
+func create_saving_throw_block() *saving_throw_block {
+	v := saving_throw_block{widgets.NewQGroupBox2("", nil), widgets.NewQComboBox(nil), widgets.NewQLabel2("Saving Throw:", nil, 0), widgets.NewQComboBox(nil), widgets.NewQLabel2("vs DC:", nil, 0), widgets.NewQComboBox(nil), widgets.NewQLabel2("Save Effect:", nil, 0), widgets.NewQLineEdit(nil), widgets.NewQGridLayout2()}
+	return &v
+}
+
+func (x saving_throw_block) setup() {
+	x.SavingThrowComboBoxTF.AddItems([]string{"False", "True"})
+	x.SavingThrowComboBoxStat.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA"})
+	x.SavingThrowComboBoxType.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA", "SPELL", "-"})
+	x.SavingThrowLineEdit.SetPlaceholderText("Half Damage")
+
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowComboBoxTF, 0, 0, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowLabelSavingThrow, 0, 1, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowComboBoxStat, 0, 2, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowLabelVsDC, 0, 3, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowComboBoxType, 0, 4, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowLabelEffect, 1, 0, 0)
+	x.SavingThrowLayout.AddWidget2(x.SavingThrowLineEdit, 1, 1, 0)
+
+	x.SavingThrowGroup.SetLayout(x.SavingThrowLayout)
+}
+
+type d20_macro interface {
+	setup()
+}
+
+type d20_macro_block struct {
+	d20_attack_block  *attack_block
+	d20_damage_block1 *damage_block
+	d20_damage_block2 *damage_block
+	d20_saving_block  *saving_throw_block
+	Layout            *widgets.QGridLayout
+}
+
+func create_d20_macro() *d20_macro_block {
+	v := d20_macro_block{create_attack_block(), create_damage_block(), create_damage_block(), create_saving_throw_block(), widgets.NewQGridLayout2()}
+	return &v
+}
+
+func (x d20_macro_block) setup() {
+	x.d20_attack_block.setup()
+	x.d20_damage_block1.setup()
+	x.d20_damage_block2.setup()
+	x.d20_saving_block.setup()
+
+	x.Layout.AddWidget2(x.d20_attack_block.AttkGroup, 1, 0, 0)
+	x.Layout.AddWidget2(x.d20_damage_block1.Damage1Group, 2, 0, 0)
+	x.Layout.AddWidget2(x.d20_damage_block2.Damage1Group, 3, 0, 0)
+	x.Layout.AddWidget2(x.d20_saving_block.SavingThrowGroup, 4, 0, 0)
+}
 
 func main() {
 	widgets.NewQApplication(len(os.Args), os.Args)
+
+	t := create_d20_macro()
+	t.setup()
+
+	v := create_d20_macro()
+	v.setup()
 
 	w := create_attack_block()
 	w.setup()
@@ -129,116 +240,14 @@ func main() {
 	mySecondTabPage = widgets.NewQWidget(nil, 0)
 
 	//echoComboBox.ConnectCurrentIndexChanged(func(index int) { echoChanged(echoLineEditCharName, index) })
+	d1 := create_damage_block()
+	d1.setup()
 
-	//Damage 1 Stuff
-	var (
-		Damage1Group          = widgets.NewQGroupBox2("", nil)
-		Damage1ComboBoxTF     = widgets.NewQComboBox(nil)
-		Damage1LabelDamage    = widgets.NewQLabel2("Damage:", nil, 0)
-		Damage1LineEditDamage = widgets.NewQLineEdit(nil)
-		Damage1Plus1          = widgets.NewQLabel2("+", nil, 0)
-		Damage1ComboBoxType   = widgets.NewQComboBox(nil)
-		Damage1LabelPlus2     = widgets.NewQLabel2("+", nil, 0)
-		Damage1LineEditBonus  = widgets.NewQLineEdit(nil)
+	d2 := create_damage_block()
+	d2.setup()
 
-		Damage1LabelType    = widgets.NewQLabel2("Type:", nil, 0)
-		Damage1LineEditType = widgets.NewQLineEdit(nil)
-		Damage1LabelCrit    = widgets.NewQLabel2("Crit:", nil, 0)
-		Damage1LineEditCrit = widgets.NewQLineEdit(nil)
-	)
-
-	Damage1ComboBoxTF.AddItems([]string{"True", "False"})
-	Damage1ComboBoxType.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA", "SPELL", "-"})
-	Damage1LineEditDamage.SetPlaceholderText("1d6")
-	Damage1LineEditBonus.SetPlaceholderText("0")
-	Damage1LineEditType.SetPlaceholderText("Slashing")
-	Damage1LineEditCrit.SetPlaceholderText("1d6")
-
-	var damage1Layout = widgets.NewQGridLayout2()
-	damage1Layout.AddWidget2(Damage1ComboBoxTF, 0, 0, 0)
-	damage1Layout.AddWidget2(Damage1LabelDamage, 0, 1, 0)
-	damage1Layout.AddWidget2(Damage1LineEditDamage, 0, 2, 0)
-	damage1Layout.AddWidget2(Damage1Plus1, 0, 3, 0)
-	damage1Layout.AddWidget2(Damage1ComboBoxTF, 0, 4, 0)
-	damage1Layout.AddWidget2(Damage1LabelPlus2, 0, 5, 0)
-	damage1Layout.AddWidget2(Damage1LineEditBonus, 0, 6, 0)
-
-	damage1Layout.AddWidget2(Damage1LabelType, 1, 0, 0)
-	damage1Layout.AddWidget2(Damage1LineEditType, 1, 1, 0)
-	damage1Layout.AddWidget2(Damage1LabelCrit, 1, 2, 0)
-	damage1Layout.AddWidget2(Damage1LineEditCrit, 1, 3, 0)
-
-	Damage1Group.SetLayout(damage1Layout)
-
-	//Damage 2 Stuff
-	var (
-		Damage2Group          = widgets.NewQGroupBox2("", nil)
-		Damage2ComboBoxTF     = widgets.NewQComboBox(nil)
-		Damage2LabelDamage    = widgets.NewQLabel2("Damage:", nil, 0)
-		Damage2LineEditDamage = widgets.NewQLineEdit(nil)
-		Damage2Plus1          = widgets.NewQLabel2("+", nil, 0)
-		Damage2ComboBoxType   = widgets.NewQComboBox(nil)
-		Damage2LabelPlus2     = widgets.NewQLabel2("+", nil, 0)
-		Damage2LineEditBonus  = widgets.NewQLineEdit(nil)
-
-		Damage2LabelType    = widgets.NewQLabel2("Type:", nil, 0)
-		Damage2LineEditType = widgets.NewQLineEdit(nil)
-		Damage2LabelCrit    = widgets.NewQLabel2("Crit:", nil, 0)
-		Damage2LineEditCrit = widgets.NewQLineEdit(nil)
-	)
-
-	Damage2ComboBoxTF.AddItems([]string{"False", "True"})
-	Damage2ComboBoxType.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA", "SPELL", "-"})
-	Damage2LineEditDamage.SetPlaceholderText("1d6")
-	Damage2LineEditBonus.SetPlaceholderText("0")
-	Damage2LineEditType.SetPlaceholderText("Slashing")
-	Damage2LineEditCrit.SetPlaceholderText("1d6")
-
-	var Damage2Layout = widgets.NewQGridLayout2()
-	Damage2Layout.AddWidget2(Damage2ComboBoxTF, 0, 0, 0)
-	Damage2Layout.AddWidget2(Damage2LabelDamage, 0, 1, 0)
-	Damage2Layout.AddWidget2(Damage2LineEditDamage, 0, 2, 0)
-	Damage2Layout.AddWidget2(Damage2Plus1, 0, 3, 0)
-	Damage2Layout.AddWidget2(Damage2ComboBoxTF, 0, 4, 0)
-	Damage2Layout.AddWidget2(Damage2LabelPlus2, 0, 5, 0)
-	Damage2Layout.AddWidget2(Damage2LineEditBonus, 0, 6, 0)
-
-	Damage2Layout.AddWidget2(Damage2LabelType, 1, 0, 0)
-	Damage2Layout.AddWidget2(Damage2LineEditType, 1, 1, 0)
-	Damage2Layout.AddWidget2(Damage2LabelCrit, 1, 2, 0)
-	Damage2Layout.AddWidget2(Damage2LineEditCrit, 1, 3, 0)
-
-	Damage2Group.SetLayout(Damage2Layout)
-
-	//Saving Throw
-	var (
-		SavingThrowGroup            = widgets.NewQGroupBox2("", nil)
-		SavingThrowComboBoxTF       = widgets.NewQComboBox(nil)
-		SavingThrowLabelSavingThrow = widgets.NewQLabel2("Saving Throw:", nil, 0)
-		SavingThrowComboBoxStat     = widgets.NewQComboBox(nil)
-		SavingThrowLabelVsDC        = widgets.NewQLabel2("vs DC:", nil, 0)
-		SavingThrowComboBoxType     = widgets.NewQComboBox(nil)
-
-		SavingThrowLabelEffect = widgets.NewQLabel2("Save Effect:", nil, 0)
-		SavingThrowLineEdit    = widgets.NewQLineEdit(nil)
-	)
-
-	SavingThrowComboBoxTF.AddItems([]string{"False", "True"})
-	SavingThrowComboBoxStat.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA"})
-	SavingThrowComboBoxType.AddItems([]string{"STR", "DEX", "CON", "INT", "WIS", "CHA", "SPELL", "-"})
-	SavingThrowLineEdit.SetPlaceholderText("Half Damage")
-
-	var SavingThrowLayout = widgets.NewQGridLayout2()
-	//SavingThrowLayout.AddWidget2(SavingThrowGroup, 0, 0, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowComboBoxTF, 0, 0, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowLabelSavingThrow, 0, 1, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowComboBoxStat, 0, 2, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowLabelVsDC, 0, 3, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowComboBoxType, 0, 4, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowLabelEffect, 1, 0, 0)
-	SavingThrowLayout.AddWidget2(SavingThrowLineEdit, 1, 1, 0)
-
-	SavingThrowGroup.SetLayout(SavingThrowLayout)
+	st := create_saving_throw_block()
+	st.setup()
 
 	// Button
 
@@ -251,20 +260,12 @@ func main() {
 	ButtonLayout.AddWidget2(button, 0, 0, 0)
 	buttonGroup.SetLayout(ButtonLayout)
 
-	// Put them together
+	myFirstTabPage.SetLayout(v.Layout)
+	mySecondTabPage.SetLayout(t.Layout)
 
-	var layout = widgets.NewQGridLayout2()
-	layout.AddWidget2(x.echoGroup, 1, 0, 0)
-	layout.AddWidget2(w.AttkGroup, 2, 0, 0)
-	layout.AddWidget2(Damage1Group, 3, 0, 0)
-	layout.AddWidget2(buttonGroup, 4, 3, 0)
-	layout.AddWidget2(Damage2Group, 4, 0, 0)
-	layout.AddWidget2(SavingThrowGroup, 5, 0, 0)
+	var mainWindowLayout = widgets.NewQGridLayout2()
 
-	myFirstTabPage.SetLayout(layout)
-
-	var mainWindowLayout = widgets.NewQVBoxLayout()
-	mainWindowLayout.AddWidget(myTabWidget, 0, core.Qt__AlignCenter)
+	mainWindowLayout.AddWidget2(myTabWidget, 0, 0, 0)
 
 	var window = widgets.NewQMainWindow(nil, 0)
 	window.SetWindowTitle("Line Edits")
